d3c419fb080029b73a655eb65a3d4c2622230509
Huey <leejinhuey@gmail.com>
2019-09-14 15:24:26 +0800

    sentry for react native web

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const createExpoWebpackConfigAsync = require(`@expo/webpack-config`)
 // Expo CLI will await this method so you can optionally return a promise.
 module.exports = async (env, argv) => {
   const config = await createExpoWebpackConfigAsync(env, argv)
+
+  // maps support for react native web
   config.resolve.alias[`react-native`] = `react-native-web`
   config.resolve.alias[`react-native-maps`] = `react-native-web-maps`
+
+  // sentry support for react native web
+  config.resolve.alias[`sentry-expo`] = `@sentry/browser`
 
   // Maybe you want to turn off compression in dev mode.
   if (config.mode === `development`) {
