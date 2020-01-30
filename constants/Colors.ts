b0bb33c82294268f6be31359ec8e17146a4b469b
Huey <leejinhuey@gmail.com>
2020-01-30 21:34:40 +0000

    typescript for constants

---
+++ constants/Colors.ts
@@ -0,0 +1,31 @@
+const black = `#000`
+const white = `#fff`
+
+export default {
+  accentColor: `#1B998B`,
+  accentColorLight: `#73CCC2`,
+  black,
+  buttonBackground: `#4BB3FD`,
+  cardBackground: white,
+  cardHeader: `#6E707F`,
+  disabledButtonBackground: `rgba(231, 231, 233, 1)`,
+  dividerLine: `#808080`,
+  errorColor: `rgba(215, 38, 61, 1)`,
+  graphCurrentBar: `rgba(27, 152, 137, 0.8)`,
+  graphCurrentTime: `rgba(228, 51, 28, 0.65)`,
+  indicatorGreen: `#6DD149`,
+  indicatorLime: `#A6BC35`,
+  indicatorOrange: `#D77026`,
+  indicatorRed: `#E4331C`,
+  indicatorYellow: `#FFB800`,
+  infoColor: `#1C5C8A`,
+  lightTextColor: `#6E707F`,
+  linkBlue: `#2980b9`,
+  oldCardBackground: `#f2f2f2`,
+  pageBackground: `#fefefe`,
+  tabBackground: `#ced0d9`,
+  textColor: `#4D5061`,
+  textInputBackground: `#eaeaea`,
+  warningColor: `#AB7627`,
+  white,
+}
