764cce5678638ebc66aaece9da1b8af20ce99dcc
Huey <leejinhuey@gmail.com>
2020-01-30 18:28:40 +0000

    introduce typescript to Colors

---
+++ constants/Colors.tsx
@@ -0,0 +1,31 @@
+const black: string = `#000`
+const white: string = `#fff`
+
+export default {
+  accentColor: `#1B998B` as string,
+  accentColorLight: `#73CCC2` as string,
+  black,
+  buttonBackground: `#4BB3FD` as string,
+  cardBackground: white,
+  cardHeader: `#6E707F` as string,
+  disabledButtonBackground: `rgba(231, 231, 233, 1)` as string,
+  dividerLine: `#808080` as string,
+  errorColor: `rgba(215, 38, 61, 1)` as string,
+  graphCurrentBar: `rgba(27, 152, 137, 0.8)` as string,
+  graphCurrentTime: `rgba(228, 51, 28, 0.65)` as string,
+  indicatorGreen: `#6DD149` as string,
+  indicatorLime: `#A6BC35` as string,
+  indicatorOrange: `#D77026` as string,
+  indicatorRed: `#E4331C` as string,
+  indicatorYellow: `#FFB800` as string,
+  infoColor: `#1C5C8A` as string,
+  lightTextColor: `#6E707F` as string,
+  linkBlue: `#2980b9` as string,
+  oldCardBackground: `#f2f2f2` as string,
+  pageBackground: `#fefefe` as string,
+  tabBackground: `#ced0d9` as string,
+  textColor: `#4D5061` as string,
+  textInputBackground: `#eaeaea` as string,
+  warningColor: `#AB7627` as string,
+  white,
+}
