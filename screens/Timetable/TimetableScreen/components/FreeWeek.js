f91b5ea1c7afb195297a3d7e581a6ab290c8087c
Huey <leejinhuey@gmail.com>
2020-05-30 10:16:30 +0800

    ⚡ IMRPOVE: FreeWeek should be a functional component

--- screens/Timetable/TimetableScreen/components/FreeWeek.js
+++ screens/Timetable/TimetableScreen/components/FreeWeek.js
@@ -3,7 +3,6 @@ import {
   Image,
   StyleSheet,
   View,
-  ViewPropTypes,
 } from 'react-native'
 
 import { CentredText } from "../../../../components/Typography"
@@ -34,37 +33,21 @@ const relaxIllustration = Random.array([
   AssetManager.undraw.relaxingAtHome,
 ])
 
-// eslint-disable-next-line react/prefer-stateless-function
-class FreeWeek extends React.Component {
-  static propTypes = {
-    style: ViewPropTypes.style,
-  }
-
-  static defaultProps = {
-    style: {},
-  }
-
-  render() {
-    const {
-      style,
-    } = this.props
-    return (
-      <View style={[styles.container, style]}>
-        <CentredText>
-          Nothing scheduled this week.
-        </CentredText>
-        <CentredText>
-          Take it easy!
-        </CentredText>
-        <Image
-          source={relaxIllustration}
-          resizeMethod="scale"
-          style={[Styles.image, styles.timetableImage]}
-          resizeMode="contain"
-        />
-      </View>
-    )
-  }
-}
+const FreeWeek = ({ style }) => (
+  <View style={[styles.container, style]}>
+    <CentredText>
+      Nothing scheduled this week.
+    </CentredText>
+    <CentredText>
+      Take it easy!
+    </CentredText>
+    <Image
+      source={relaxIllustration}
+      resizeMethod="scale"
+      style={[Styles.image, styles.timetableImage]}
+      resizeMode="contain"
+    />
+  </View>
+)
 
 export default FreeWeek
