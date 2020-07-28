7e88f970a3b1d1c3e52dac109138215e3a6e128d
Huey <hello@huey.xyz>
2020-07-28 11:52:39 +0800

    cleanup

--- jest/setupTests.ts
+++ jest/setupTests.ts
@@ -1,10 +1,6 @@
-// https://github.com/testing-library/jest-native/issues/25
-// import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock'
 import '@testing-library/jest-native/extend-expect'
 import '@testing-library/react-native/cleanup-after-each'
 import 'react-native-gesture-handler/jestSetup'
-
-// jest.mock(`@react-native-community/async-storage`, () => mockAsyncStorage)
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because
 // the native animated module is missing
