b419f01714f4e203e568ae1483cc77072aa66898
Huey <hello@huey.xyz>
2020-09-28 12:37:05 +0800

    show buttons when loading

--- screens/Timetable/TimetableScreen/components/LoadingTimetable.tsx
+++ screens/Timetable/TimetableScreen/components/LoadingTimetable.tsx
@@ -1,10 +1,21 @@
+import { Moment } from 'moment'
 import React, { ReactElement } from 'react'
 import { ActivityIndicator, StyleSheet, View } from 'react-native'
-
 import { PageNoScroll } from '../../../../components/Containers'
 import { BodyText } from '../../../../components/Typography'
+import DateControls from './DateControls'
+
+interface Props {
+  date: Moment,
+  onDateChanged: (date: Moment) => Promise<void>,
+  onIndexChanged: (index: number) => void,
+}
 
 const styles = StyleSheet.create({
+  header: {
+    alignItems: `center`,
+    marginBottom: 20,
+  },
   loadingText: {
     marginBottom: 20,
   },
@@ -18,8 +29,19 @@ const styles = StyleSheet.create({
   },
 })
 
-const LoadingTimetable = (): ReactElement => (
+const LoadingTimetable: React.FC<Props> = ({
+  date,
+  onDateChanged,
+  onIndexChanged,
+}): ReactElement => (
   <PageNoScroll style={styles.pageContainer}>
+    <View style={styles.header}>
+      <DateControls
+        date={date}
+        onDateChanged={onDateChanged}
+        onIndexChanged={onIndexChanged}
+      />
+    </View>
     <View style={styles.messageContainer}>
       <BodyText style={styles.loadingText}>Loading timetable...</BodyText>
       <ActivityIndicator size="large" />
